b28a32e3a88c1257ec92ca8bb0f56d1c8f0aaa28
YOYOPIG <yoyosuperpig@gmail.com>
2019-11-18 20:04:09 +0800

    Add template file

---
+++ test.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <fstream>
+#include <queue>
+#include <stack>
+#include <set>
+#include <map>
+#include <stdio.h>
+#include <string.h>
+#include <utility>
+#include <math.h>
+
+using namespace std;
+
+void realMain()
+{
+	return;
+}
+
+int main() 
+{
+    int T;
+	cin >> T;
+	for(int t=0;t<T;++t)
+	{
+	    int nums[9];
+	    for(int n=0;n<9;++n)
+	        cin>>nums[n];
+	    cout<<"Case #"<<t+1<<": ";
+	    realMain(); 
+	}
+	return 0;
+}
